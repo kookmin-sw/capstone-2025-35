19ca2999c1f6ed6106f0aca51aa49cefd4525643
iamjames77 <iamjames77@daum.net>
2025-03-09 14:44:34 +0900

    구조 약간 변경

--- BE/config.py
+++ BE/config.py
@@ -2,6 +2,6 @@
 UPDATE_MAC_INTERVAL = 10  # MAC 주소 갱신 주기 (초)
 DISC_RANGE = 13  # 이산화 구간
 INTERFACE = "en0"  # 네트워크 인터페이스
-MONITORING_IP_LIST = ["10.223.122.179"]
-SNIFF_LIB = "pyshark"
+MONITORING_IP_LIST = ["10.138.53.67"]
+SNIFF_LIB = "scapy"
 BITMAP_PATH = "bitmap_record.pkl"
