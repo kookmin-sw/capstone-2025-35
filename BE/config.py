3c45f0c3bae7e7ca568f22017582e5bf4b42ee9e
iamjames77 <iamjames77@daum.net>
2025-03-09 13:33:03 +0900

    json 파일을 읽는 형식이 아닌 config.py 파일을 읽는 형식으로 변경

---
+++ BE/config.py
@@ -0,0 +1,7 @@
+# config.py
+UPDATE_MAC_INTERVAL = 10  # MAC 주소 갱신 주기 (초)
+DISC_RANGE = 13  # 이산화 구간
+INTERFACE = "en0"  # 네트워크 인터페이스
+MONITORING_IP_LIST = ["10.223.122.179"]
+SNIFF_LIB = "pyshark"
+BITMAP_PATH = "bitmap_record.pkl"
