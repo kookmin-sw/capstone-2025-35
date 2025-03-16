dd307358fdc45937bb77b33c496866fac425238c
iamjames77 <iamjames77@daum.net>
2025-03-17 00:54:22 +0900

    config.py

---
+++ BE/config.py
@@ -0,0 +1,17 @@
+# config.py
+UPDATE_MAC_INTERVAL = 10  # MAC 주소 갱신 주기 (초)
+DISC_RANGE = 13  # 이산화 구간
+INTERFACE = "en0"  # 네트워크 인터페이스
+MONITORING_IP_LIST = ["192.168.219.171"]
+MONITORING_PERIOD = 20  # 모니터링 주기 (초)
+SNIFF_LIB = "pyshark"
+BITMAP_PATH = "bitmap_record.pkl"
+LOG_PATH = "../logs"
+TARGET_APPLICATIONS = {
+    "googlevideo.com" : "youtube",
+    "nflxvideo.net" : "netflix",
+    "smartmediarep.com": "navertv",
+    "naver-vod.pstatic.net": "navertv",
+    "vod.cdn.wavve.com": "wavve",
+    "vod02-cosmos.coupangstreaming.com": "coupangplay",
+}
