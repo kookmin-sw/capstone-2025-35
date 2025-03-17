ab93418b7b3625bd86ab6cb663bb8d538ba0fc02
iamjames77 <iamjames77@daum.net>
2025-03-17 20:03:49 +0900

    config

--- BE/config.py
+++ BE/config.py
@@ -2,7 +2,7 @@
 UPDATE_MAC_INTERVAL = 10  # MAC 주소 갱신 주기 (초)
 DISC_RANGE = 13  # 이산화 구간
 INTERFACE = "en0"  # 네트워크 인터페이스
-MONITORING_IP_LIST = ["192.168.219.105"]
+MONITORING_IP_LIST = ["192.168.219.171"]
 MONITORING_PERIOD = 20  # 모니터링 주기 (초)
 SNIFF_LIB = "pyshark"
 BITMAP_PATH = "bitmap_school.pkl"
