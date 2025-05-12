123aeee5b2576acbc448c48b600855c2f833ebdc
Jang2019 <54920299+Jang2019@users.noreply.github.com>
2025-05-12 22:09:08 +0900

    Update auto_laftel.py

--- BE/auto_flow/auto_laftel.py
+++ BE/auto_flow/auto_laftel.py
@@ -12,15 +12,15 @@ from selenium.webdriver.support import expected_conditions as EC
 # 웹드라이버 경로 설정
 service = Service('/usr/local/bin/chromedriver')  # 경로 확인
 
-save_dir = '/home/jang/Documents/new_pcap/capstone-2025-35/pcap/laftel/PC/WiFi'  # 원하는 pcap 경로로 수정
+save_dir = ''  # 원하는 경로로 수정
 
 # TShark 실행 (현재는 주석 처리)
 timestamp = time.strftime("%Y%m%d_%H%M%S")
-# 전체 파일 경로 생성 이름
-pcap_path = os.path.join(save_dir, f'{timestamp}_Jang_laftel.pcap')
+# 전체 파일 경로 생성
+pcap_path = os.path.join(save_dir, f'{timestamp}_laftel.pcap')
 
 # tshark 실행
-tshark_process = subprocess.Popen(['tshark', '-i', 'wlp61s0', '-w', pcap_path])
+tshark_process = subprocess.Popen(['tshark', '-i', '네트워크 인터페이스', '-w', pcap_path])
 print(f"PCAP 파일 저장 위치: {pcap_path}")
 
 options = Options()  # ChromeOptions 인스턴스 생성
@@ -28,7 +28,7 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-software-rasterizer")
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--headless")
+#options.add_argument("--headless")
 
 try:
 
@@ -97,41 +97,39 @@ try:
         time.sleep(4)
 
         # <a> 태그 요소 찾기 (클래스명을 사용하여)
-        link = driver.find_element(By.CSS_SELECTOR, '.sc-a4352714-11.capRUh')
-
+        link = driver.find_element(By.LINK_TEXT, '태그검색')
         # 클릭
         link.click()
 
-        time.sleep(4)
+        time.sleep(3)
 
-        # 'sc-ed8262e4-0 jgcDvs' 클래스를 가진 모든 요소 찾기
-        elements = driver.find_elements(By.CSS_SELECTOR, '.sc-ed8262e4-0.jgcDvs')
+        # href 속성에 modal=이 포함된 모든 요소를 찾기
+        wait = WebDriverWait(driver, 10)
+        elements = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@href, 'modal=')]")))
 
-        # 요소가 존재하면 랜덤으로 클릭
-        if elements:
-            random_element = random.choice(elements)  # 랜덤으로 요소 선택
-            random_element.click()
-            print("요소를 클릭했습니다.")
-        else:
-            print("대상 요소를 찾을 수 없습니다.")
+        # 랜덤으로 하나 선택하여 클릭
+        random_element = random.choice(elements)
+        random_element.click()
 
         time.sleep(3)
 
-        # 'sc-472bf40-2 kRUErM' 클래스의 영상 선택
-        elements = driver.find_elements(By.CSS_SELECTOR, '.sc-472bf40-2.kRUErM')
-        if elements:
-            random.choice(elements).click()
-            print("영상 시청 시작")
-        else:
-            print("대상 영상 요소를 찾을 수 없습니다.")
+        # 모든 episode-item 요소 수집
+        episodes = driver.find_elements(By.CSS_SELECTOR, 'a[data-cy="episode-item"]')
 
-        time.sleep(20)
+        # 요소가 비어있지 않은지 확인
+        if episodes:
+            random_episode = random.choice(episodes)
+            random_episode.click()
+        else:
+            print("에피소드 요소를 찾을 수 없습니다.")
+
+        time.sleep(23)
 
         # 링크 클릭 (SVG 아이콘을 포함한 <a> 태그)
         link = driver.find_element(By.CSS_SELECTOR, 'a[href="/"]')
         link.click()
 
-        time.sleep(5)
+        time.sleep(3)
 
 except Exception as e:
     print("오류 발생:", e)
@@ -145,3 +143,4 @@ finally:
     # tshark_process.kill()     # 강제 종료 (terminate가 안 통할 경우)
     tshark_process.wait()       # 종료될 때까지 대기
     print("프로그램 종료")
+
